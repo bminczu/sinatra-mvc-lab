6e52d5d7c8c8206669c52c1466ac262248fa1d21
bminczu <bminczu@gmail.com>
2020-11-18 12:10:53 -0600

    commit tree 224d6ccc908bd4fc5da4bedefb78a47ff797bb7f

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        erb :user_input
+    end
+
+ 
+    post '/piglatinize' do
+        piglatin = Piglatinizer.new
+        @user_pl_text = piglatin(piglatinize(params[:user_Phrase]))
+        
+        erb :result
+      
+    end
 end
